61a2b8fded999bd4b386f1f85ce6089dc3668639
DESKTOP-385RQJR\Fox <fox.spa@gmail.com>
2020-10-11 19:42:01 +0200

    fix get component

--- module_injector/src/main/java/com/example/module_injector/ComponentHolderImpl.kt
+++ module_injector/src/main/java/com/example/module_injector/ComponentHolderImpl.kt
@@ -15,11 +15,11 @@ class ComponentHolderDelegate<A : BaseAPI, D : BaseDependencies, C : A>(
 
         synchronized(this) {
             dependencyProvider?.let { dependencyProvider ->
-                if (componentRef?.get() == null) {
-                    componentRef =
-                        WeakReference(componentFactory(dependencyProvider()))
-                }
                 component = componentRef?.get()
+                if (component == null) {
+                    component = componentFactory(dependencyProvider())
+                    componentRef = WeakReference(component)
+                }
             } ?: throw IllegalStateException("dependencyProvider for component with factory $componentFactory is not initialized")
         }
 
